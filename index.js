57d49231db69d7f010756bee61bfb90e959f56af
buariful <94697522+buariful@users.noreply.github.com>
2023-01-10 21:49:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 // middlewares
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config({ path: "backend/config.env" });
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
